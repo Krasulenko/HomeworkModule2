1e0b00d06ec633e924d3efb5f128a0c5fc5d6360
Krasulenko <krasul@ukr.net>
2016-08-29 03:53:57 +0300

    Commit Homework24

---
+++ src/Homework24.java
@@ -0,0 +1,25 @@
+
+public class Homework24 {
+
+    static  double[] balances = {1200, 250, 2000, 500, 3200};
+    static  String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+    static double fundBalance(String ownerName, double fund){
+        double balance = 0;
+        for (int i = 0; i < ownerNames.length; i++){
+            if (ownerNames[i] == ownerName)
+                balance = balances[i];
+        }
+        return balance + fund;
+    }
+
+    public static void main(String[] args) {
+
+        String ownerName = "Lane";
+        double fund = 800;
+
+        double balanceAfter = fundBalance(ownerName,fund);
+        System.out.println(ownerName + " " + balanceAfter);
+    }
+
+}
